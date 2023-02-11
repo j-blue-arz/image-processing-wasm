58859fbdbf396d04d6a8bb8531e806a3a204a0a6
Julian Arz <j.arz@web.de>
2023-02-12 00:57:56 +0100

    refactors python app
    * removes globals

--- image-processing-webservice/app.py
+++ image-processing-webservice/app.py
@@ -3,39 +3,33 @@ import sys
 from wasmer import engine, wasi, Store, Module, Instance
 from wasmer_compiler_cranelift import Compiler
 
-instance = None
-wasm_file = "../operators/noop.wasm"
+def create_instance(wasm_file):
+    with open(wasm_file, "rb") as f:
+        wasm_bytes = f.read()
 
-def get_instance():
-    global instance
-    if not instance:
-        with open(wasm_file, "rb") as f:
-            wasm_bytes = f.read()
+    store = Store(engine.Universal(Compiler))
+    module = Module(store, wasm_bytes)
 
-        store = Store(engine.Universal(Compiler))
-        module = Module(store, wasm_bytes)
+    wasi_version = wasi.get_version(module, strict=True)
 
-        wasi_version = wasi.get_version(module, strict=True)
+    wasi_env = wasi.StateBuilder("image-processor").finalize()
 
-        wasi_env = wasi.StateBuilder("image-processor").finalize()
+    import_object = wasi_env.generate_import_object(store, wasi_version)
 
-        import_object = wasi_env.generate_import_object(store, wasi_version)
+    return Instance(module, import_object)
 
-        instance = Instance(module, import_object)
-    return instance
-
-def read_image():
+def read_image(instance):
     with open("skyline.jpg", "rb") as f:
         image_bytes = f.read()
 
-    buffer_pointer = get_instance().exports.getInputBuffer(len(image_bytes))
-    buffer = get_instance().exports.memory.uint8_view(offset=buffer_pointer)
+    buffer_pointer = instance.exports.getInputBuffer(len(image_bytes))
+    buffer = instance.exports.memory.uint8_view(offset=buffer_pointer)
     buffer[:len(image_bytes)] = image_bytes
 
-def write_image(buffer_pointer):
+def write_image(instance, buffer_pointer):
     if buffer_pointer != 0:
-        size = get_instance().exports.getOutputBufferSize(buffer_pointer)
-        buffer = get_instance().exports.memory.uint8_view(offset=buffer_pointer)
+        size = instance.exports.getOutputBufferSize(buffer_pointer)
+        buffer = instance.exports.memory.uint8_view(offset=buffer_pointer)
 
         image_bytes = bytes(buffer[:size])
 
@@ -45,8 +39,8 @@ def write_image(buffer_pointer):
         print("aborting")
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        wasm_file = sys.argv[1]
-    read_image()
-    buffer_pointer = get_instance().exports.applyImageOperator()
-    write_image(buffer_pointer)
+    wasm_file = sys.argv[1] if len(sys.argv) > 1 else "../operators/noop.wasm"
+    instance = create_instance(wasm_file)
+    read_image(instance)
+    buffer_pointer = instance.exports.applyImageOperator()
+    write_image(instance, buffer_pointer)
