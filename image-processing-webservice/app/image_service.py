71e7af55b910b98da1ce89fe74a143fa248f0e43
Julian Arz <julian.arz@codecentric.de>
2023-02-17 16:52:00 +0100

    wip: send/receive image in flask

--- image-processing-webservice/app/image_service.py
+++ image-processing-webservice/app/image_service.py
@@ -1,10 +1,21 @@
-from flask import Blueprint, request
-from operators import apply_operator
+from flask import Blueprint, request, make_response
+from . import operators
 
 API = Blueprint("api", __name__, url_prefix='/api')
+
+@API.route("/info", methods=["GET"])
+def version():
+    return "0.1.0"
 
 @API.route("/process-image", methods=["POST"])
 def process_image():
     imagefile = request.files['imagefile']
-    result = apply_operator(imagefile.read())
+    image_bytes = operators.apply_operator(imagefile.read())
+    response = make_response(image_bytes)
+    response.headers.set('Content-Type', 'image/jpeg')
+    response.headers.set(
+        'Content-Disposition', 'attachment', filename='%s.jpg' % pid)
+    return response
+
+
 
