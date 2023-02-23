4d41f820c38f03cd323581f54b37a5b6c2632c7a
Julian Arz <julian.arz@codecentric.de>
2023-02-23 17:18:32 +0100

    wip: integrate wasmtime
    unclear how to support wasi with wasmtime-python

--- image-processing-webservice/app/runtimes/wasmtime.py
+++ image-processing-webservice/app/runtimes/wasmtime.py
@@ -2,24 +2,27 @@ from wasmtime import WasiConfig, Store, Module, Instance
 
 class WasmtimeRuntime:
     def __init__(self, wasm_file):
-        store = Store()
-        store.set_wasi(WasiConfig())
-        module = Module.from_file(store.engine, wasm_file)
-        self._instance = Instance(store, module, [])
+        self._store = Store()
+        self._store.set_wasi(WasiConfig())
+        module = Module.from_file(self._store.engine, wasm_file)
+        self._instance = Instance(self._store, module, [])
         self._buffer_pointer = 0
     
     def read_image_into_memory(self, image_bytes):
-        buffer_pointer = self._instance.exports.getInputBuffer(len(image_bytes))
-        buffer = self._instance.exports.memory.uint8_view(offset=buffer_pointer)
+        buffer_pointer = self._exports("getInputBuffer")(len(image_bytes))
+        buffer = self._exports("memory").uint8_view(offset=buffer_pointer)
         buffer[:len(image_bytes)] = image_bytes
+
+    def _exports(self, name):
+        return self._instance.exports(self._store)[name]
     
     def apply_operator(self):
-        self._buffer_pointer = self._instance.exports.applyImageOperator()
+        self._buffer_pointer = self._exports("applyImageOperator")()
     
     def retrieve_image(self):
         if self._buffer_pointer != 0:
-            size = self._instance.exports.getOutputBufferSize(self._buffer_pointer)
-            buffer = self._instance.exports.memory.uint8_view(offset=self._buffer_pointer)
+            size = self._exports("getOutputBufferSize")(self._buffer_pointer)
+            buffer = self._exports("memory").uint8_view(offset=self._buffer_pointer)
 
             return bytes(buffer[:size])
         else:
